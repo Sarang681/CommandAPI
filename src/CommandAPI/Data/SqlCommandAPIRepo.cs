3623ec59870246ece3b12f6f8db1728eed86b3ce
Sarang837 <f20190594@goa.bits-pilani.ac.in>
2023-07-06 19:36:18 +0530

    updated repo and added env variables

---
+++ src/CommandAPI/Data/SqlCommandAPIRepo.cs
@@ -0,0 +1,43 @@
+using CommandAPI.Models;
+
+namespace CommandAPI.Data
+{
+    public class SqlCommandAPIRepo : ICommandAPIRepo
+    {
+        private readonly CommandContext _context;
+
+        public SqlCommandAPIRepo(CommandContext context)
+        {
+            _context = context;
+        }
+        public void CreateCommand(Command command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCommand(Command command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return _context.CommandItems.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return _context.CommandItems.FirstOrDefault(cmd => cmd.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommand(Command command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,9 +1,20 @@
 using CommandAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var strBuilder = new NpgsqlConnectionStringBuilder(builder.Configuration.GetConnectionString("PostgreSqlConnection"))
+{
+    Username = builder.Configuration["UserID"],
+    Password = builder.Configuration["Password"]
+};
+builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(
+    strBuilder.ConnectionString
+));
+
 builder.Services.AddControllers();
-builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
 
 var app = builder.Build();
 
